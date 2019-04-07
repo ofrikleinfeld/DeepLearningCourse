probably fixed bug in backprop but still converges very very slowly

HW1/Models.py
@@ -13,7 +13,7 @@ class FeedForwardNet(object):
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y) for y in sizes[1:]]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
         self.z_values = [None] * self.num_layers
         self.activations = [None] * self.num_layers
@@ -36,22 +36,22 @@ class FeedForwardNet(object):
 
         # feed forward until one layer before final layer
         for l in range(self.num_layers - 2):
-            self.z_values[l+1] = self.activations[l] @ self.weights[l].transpose() + self.biases[l]
+            self.z_values[l+1] = self.weights[l] @ self.activations[l] + self.biases[l]
             self.activations[l+1] = sigmoid(self.z_values[l+1])
 
         # for final layer, activation is softmax. Will be performed during backprop
-        self.z_values[-1] = self.activations[-2] @ self.weights[-1].transpose() + self.biases[-1]
+        self.z_values[-1] = self.weights[-1] @ self.activations[-2] + self.biases[-1]
 
         return self.z_values[-1]
 
     def compute_gradients(self, y_batch):
         delta = [None] * (self.num_layers - 1)
         delta[-1] = softmax(self.z_values[-1]) - y_batch
-        self.dw[-1] = delta[-1].reshape(-1, delta[-1].shape[1], 1) @ self.activations[-2].reshape(-1, 1, self.activations[-2].shape[1])
+        self.dw[-1] = delta[-1] @ self.activations[-2].transpose(0, 2, 1)
 
         for l in range(2, self.num_layers):
-            delta[-l] = delta[-l+1] @ self.weights[-l+1] * sigmoid_derivative(self.z_values[-l])
-            self.dw[-l] = delta[-l].reshape(-1, delta[-l].shape[1], 1) @ self.activations[-l-1].reshape(-1, 1, self.activations[-l-1].shape[1])
+            delta[-l] = self.weights[-l+1].transpose() @ delta[-l+1] * sigmoid_derivative(self.z_values[-l])
+            self.dw[-l] = delta[-l] @ self.activations[-l-1].transpose(0, 2, 1)
 
         self.db = delta
         return self.dw, self.db
@@ -59,9 +59,9 @@ class FeedForwardNet(object):
     def predict_batch(self, x_batch):
         a = x_batch
         for l in range(self.num_layers - 2):
-            z = a @ self.weights[l].transpose() + self.biases[l]
+            z = self.weights[l] @ a + self.biases[l]
             a = sigmoid(z)
 
-        output = a @ self.weights[-1].transpose() + self.biases[-1]
+        output = self.weights[-1] @ a + self.biases[-1]
         return np.argmax(softmax(output), axis=1)
 

HW1/input_data_processing.py
@@ -23,11 +23,11 @@ def load():
     f = gzip.open('./data/mnist.pkl.gz', 'rb')
     train_from_file, validation_from_file, test_from_file = pickle.load(f, encoding='iso-8859-1')
     f.close()
-    training_inputs = np.array([np.reshape(x, 784) for x in train_from_file[0]])
+    training_inputs = np.array([np.reshape(x, (784, 1)) for x in train_from_file[0]])
     training_labels = np.array([vectorized_result(y) for y in train_from_file[1]])
-    validation_inputs = np.array([np.reshape(x, 784) for x in validation_from_file[0]])
+    validation_inputs = np.array([np.reshape(x, (784, 1)) for x in validation_from_file[0]])
     validation_labels = np.array([vectorized_result(y) for y in validation_from_file[1]])
-    test_inputs = np.array([np.reshape(x, 784) for x in test_from_file[0]])
+    test_inputs = np.array([np.reshape(x, (784, 1)) for x in test_from_file[0]])
     test_labels = np.array([vectorized_result(y) for y in test_from_file[1]])
     return training_inputs, training_labels, validation_inputs, validation_labels, test_inputs, test_labels
 
@@ -37,6 +37,6 @@ def vectorized_result(j):
     position and zeroes elsewhere.  This is used to convert a digit
     (0...9) into a corresponding desired output from the neural
     network."""
-    e = np.zeros(10)
+    e = np.zeros((10, 1))
     e[j] = 1.0
     return e