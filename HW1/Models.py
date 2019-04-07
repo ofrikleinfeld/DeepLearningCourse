entire pipeline. probably bug in backpropogation implementation - network with relu explodes, network with sigmoid doesn't converage beyond random guess

HW1/Models.py
@@ -1,13 +1,19 @@
 import numpy as np
 
-from util_functions import relu, relu_derivative, softmax
+from util_functions import relu, relu_derivative, softmax, sigmoid, sigmoid_derivative
 
 
 class FeedForwardNet(object):
+
+    @staticmethod
+    def get_num_correct_predictions(predictions, labels):
+        correct_label = np.argmax(labels, axis=1)
+        return np.sum(predictions == correct_label)
+
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.biases = [np.random.randn(y) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
         self.z_values = [None] * self.num_layers
         self.activations = [None] * self.num_layers
@@ -27,29 +33,35 @@ class FeedForwardNet(object):
     def forward_pass(self, x_batch):
         self.z_values[0] = x_batch
         self.activations[0] = x_batch
-        for l in range(self.num_layers - 1):
-            self.z_values[l+1] = np.dot(self.weights[l], self.activations[l]) + self.biases[l]
-            self.activations[l+1] = relu(self.z_values[l+1])
+
+        # feed forward until one layer before final layer
+        for l in range(self.num_layers - 2):
+            self.z_values[l+1] = self.activations[l] @ self.weights[l].transpose() + self.biases[l]
+            self.activations[l+1] = sigmoid(self.z_values[l+1])
+
+        # for final layer, activation is softmax. Will be performed during backprop
+        self.z_values[-1] = self.activations[-2] @ self.weights[-1].transpose() + self.biases[-1]
 
         return self.z_values[-1]
 
     def compute_gradients(self, y_batch):
         delta = [None] * (self.num_layers - 1)
         delta[-1] = softmax(self.z_values[-1]) - y_batch
-        self.dw[-1] = np.dot(delta[-1], self.activations[-2].transpose())
+        self.dw[-1] = delta[-1].reshape(-1, delta[-1].shape[1], 1) @ self.activations[-2].reshape(-1, 1, self.activations[-2].shape[1])
 
         for l in range(2, self.num_layers):
-            delta[-l] = np.dot(self.weights[-l+1].transpose(), delta[-l+1]) * relu_derivative(self.z_values[-l])
-            self.dw[-l] = np.dot(delta[-l], self.activations[-l-1].transpose())
+            delta[-l] = delta[-l+1] @ self.weights[-l+1] * sigmoid_derivative(self.z_values[-l])
+            self.dw[-l] = delta[-l].reshape(-1, delta[-l].shape[1], 1) @ self.activations[-l-1].reshape(-1, 1, self.activations[-l-1].shape[1])
 
         self.db = delta
         return self.dw, self.db
 
     def predict_batch(self, x_batch):
         a = x_batch
-        for l in range(self.num_layers - 1):
-            z = np.dot(self.weights[l], a) + self.biases[l]
-            a = relu(z)
+        for l in range(self.num_layers - 2):
+            z = a @ self.weights[l].transpose() + self.biases[l]
+            a = sigmoid(z)
 
-        output = np.dot(self.weights[-1], a) + self.biases[-1]
+        output = a @ self.weights[-1].transpose() + self.biases[-1]
         return np.argmax(softmax(output), axis=1)
+

HW1/Optimizers.py
@@ -19,8 +19,8 @@ class SGDOptimizer(object):
                 biases = [b - self.weights_decay_rate * b for b in biases]
 
         # perform SGD step with average batch gradient
-        new_weights = [w - self.lr * np.sum(dw) / batch_size for w, dw in zip(weights, dw)]
-        new_biases = [b - self.lr * np.sum(db) / batch_size for b, db in zip(biases, db)]
+        new_weights = [w - self.lr * np.sum(dw, axis=0) / batch_size for w, dw in zip(weights, dw)]
+        new_biases = [b - self.lr * np.sum(db, axis=0) / batch_size for b, db in zip(biases, db)]
 
         return new_weights, new_biases
 

HW1/input_data_processing.py
@@ -4,7 +4,7 @@ import gzip
 import numpy as np
 
 
-def load(train_size, test_size):
+def load():
     """Return a tuple containing ``(training_data, test_data)``.
     In particular, ``train_from_file`` is a list containing 50,000
     2-tuples ``(x, y)``.  ``x`` is a 784-dimensional numpy.ndarray
@@ -21,19 +21,15 @@ def load(train_size, test_size):
     Hence, we're using different label formats for
     the training data and the test data.  """
     f = gzip.open('./data/mnist.pkl.gz', 'rb')
-    # train_from_file, validation_from_file, test_from_file = pickle.load(f, encoding='iso-8859-1')
-    train_from_file, validation_from_file, test_from_file = pickle.load(f)
+    train_from_file, validation_from_file, test_from_file = pickle.load(f, encoding='iso-8859-1')
     f.close()
-    training_inputs = [np.reshape(x, (784, 1)) for x in train_from_file[0]]
-    training_results = [vectorized_result(y) for y in train_from_file[1]]
-    training_data = list(zip(training_inputs, training_results))
-    test_inputs = [np.reshape(x, (784, 1)) for x in test_from_file[0]]
-    test_data = list(zip(test_inputs, test_from_file[1]))
-    training_idx = np.random.choice(50000, train_size, replace=False)
-    training_data = [training_data[i] for i in training_idx]
-    test_idx = np.random.choice(10000, test_size, replace=False)
-    test_data = [test_data[i] for i in test_idx]
-    return training_data, test_data
+    training_inputs = np.array([np.reshape(x, 784) for x in train_from_file[0]])
+    training_labels = np.array([vectorized_result(y) for y in train_from_file[1]])
+    validation_inputs = np.array([np.reshape(x, 784) for x in validation_from_file[0]])
+    validation_labels = np.array([vectorized_result(y) for y in validation_from_file[1]])
+    test_inputs = np.array([np.reshape(x, 784) for x in test_from_file[0]])
+    test_labels = np.array([vectorized_result(y) for y in test_from_file[1]])
+    return training_inputs, training_labels, validation_inputs, validation_labels, test_inputs, test_labels
 
 
 def vectorized_result(j):
@@ -41,6 +37,6 @@ def vectorized_result(j):
     position and zeroes elsewhere.  This is used to convert a digit
     (0...9) into a corresponding desired output from the neural
     network."""
-    e = np.zeros((10, 1))
+    e = np.zeros(10)
     e[j] = 1.0
     return e

HW1/util_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
@@ -18,5 +19,22 @@ def relu_derivative(x):
 
 
 def softmax(x):
-    x_exp = np.exp(x)
+    x_exp = np.exp(x - np.max(x))
     return x_exp / np.sum(x_exp)
+
+
+def shuffle_dataset(data, labels):
+    assert len(data) == len(labels)
+    random_indices = np.random.permutation(len(data))
+    return data[random_indices], labels[random_indices]
+
+
+def plot_accuracy_graph(train_accuracy, test_accuracy):
+    epochs = range(1, len(train_accuracy)+1)
+    plt.plot(epochs, train_accuracy, label='Training data')
+    plt.plot(epochs, test_accuracy, label='Validation data')
+    plt.legend(loc='best')
+    plt.ylim([0, 1])
+    plt.xlabel('Epoch')
+    plt.ylabel('Prediction Accuracy')
+    plt.savefig('training_graph.pdf')