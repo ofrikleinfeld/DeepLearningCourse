fixed bug regarding softmax in different batch sizes, adds model with dropout

HW1/Models.py
@@ -50,8 +50,6 @@ class FeedForwardNet(object):
         self.dw[-1] = delta[-1] @ self.activations[-2].transpose(0, 2, 1)
 
         for l in range(2, self.num_layers):
-            a = self.weights[-l+1].transpose() @ delta[-l+1]
-            b = sigmoid_derivative(self.z_values[-l])
             delta[-l] = self.weights[-l+1].transpose() @ delta[-l+1] * sigmoid_derivative(self.z_values[-l])
             self.dw[-l] = delta[-l] @ self.activations[-l-1].transpose(0, 2, 1)
 
@@ -67,3 +65,39 @@ class FeedForwardNet(object):
         output = self.weights[-1] @ a + self.biases[-1]
         return np.argmax(softmax(output), axis=1)
 
+
+class DropoutFeedForwardNet(FeedForwardNet):
+
+    def __init__(self, sizes, dropout_rate):
+        super(DropoutFeedForwardNet, self).__init__(sizes)
+        self.dropout_rate = dropout_rate
+
+    def forward_pass(self, x_batch):
+        self.z_values[0] = x_batch
+        self.activations[0] = x_batch  # no dropout at input layer
+
+        # feed forward until one layer before final layer
+        for l in range(self.num_layers - 2):
+            self.z_values[l + 1] = self.weights[l] @ self.activations[l] + self.biases[l]
+            activations = sigmoid(self.z_values[l + 1])
+            # add dropout layer
+            dropout_layer = self.draw_dropout_layer(activations.shape)
+            self.activations[l + 1] = activations * dropout_layer
+
+        # for final layer, activation is softmax. Will be performed during backprop
+        # no dropout for final (prediction) layer
+        self.z_values[-1] = self.weights[-1] @ self.activations[-2] + self.biases[-1]
+
+    def predict_batch(self, x_batch):
+        a = x_batch
+        for l in range(self.num_layers - 2):
+            z = self.weights[l] @ a + self.biases[l]
+            a = sigmoid(z) * (1 - self.dropout_rate)
+
+        output = self.weights[-1] @ a + self.biases[-1]  # no dropout on last layer just softmax
+        return np.argmax(softmax(output), axis=1)
+
+    def draw_dropout_layer(self, shape):
+        drop_probabilities = np.random.rand(*shape)
+        dropout_layer = np.where(drop_probabilities <= self.dropout_rate, 0, 1)
+        return dropout_layer

HW1/main.py
@@ -2,16 +2,18 @@ import numpy as np
 
 from input_data_processing import load
 from util_functions import shuffle_dataset, plot_accuracy_graph
-from Models import FeedForwardNet
+from Models import FeedForwardNet, DropoutFeedForwardNet
 from Optimizers import SGDOptimizer
 
 np.random.seed(123)
 
 train_data, train_labels, validation_data, validation_labels, test_data, test_labels = load()
 net = FeedForwardNet([784, 40, 10])
-optimizer = SGDOptimizer(lr=0.0007)
+# net = DropoutFeedForwardNet(sizes=[784, 40, 10], dropout_rate=0.5)
+# optimizer = SGDOptimizer(lr=0.01, weights_decay='L2', weights_decay_rate=0.0001)
+optimizer = SGDOptimizer(lr=0.01)
 n_epochs = 15
-batch_size = 10
+batch_size = 1
 train_accuracy = []
 validation_accuracy = []
 

HW1/util_functions.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
-    # b = np.max(x)
+    # b = np.max(x, axis=1)[:, np.newaxis]
     # y = np.exp(x - b)
-    # return y / y.sum()
+    # return y / np.sum(y, axis=1)[:, np.newaxis]
     z = np.where(x >= 0, 1 / (1 + np.exp(-x)), np.exp(x) / (1 + np.exp(x)))
     return z
+
 
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
@@ -22,10 +23,7 @@ def relu_derivative(x):
 
 
 def softmax(x):
-    # x_exp = np.exp(x - np.max(x, axis=1))
-    # z = x_exp / np.sum(x_exp, axis=0)
-    # return z
-    z = x - np.max(x, axis=1)
+    z = x - np.max(x, axis=1)[:, np.newaxis]
     numerator = np.exp(z)
     denominator = np.sum(numerator, axis=1)
     softmax = numerator / denominator[:, np.newaxis]