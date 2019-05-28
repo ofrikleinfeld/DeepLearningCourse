988825bd97a6854cf97b41b7dcb09a77893e81b1
Ofri Kleinfeld <ofri64@gmail.com>
2019-05-28 22:20:58 +0300

    adds implementation of probabilistic model - ending up with only invalid paths

--- SearchAndCapture/main.py
+++ SearchAndCapture/main.py
@@ -2,7 +2,7 @@ import random
 
 import data_processing
 from search_capture_grid import WeightRankRewardGrid, WeightRankGrid
-from thief_algorithms import GreedyPathThief
+from thief_algorithms import GreedyPathThief, ProbabilisticGreedyPathThief
 
 states = data_processing.load_grid("grid_file.csv")
 init_states = data_processing.load_states_info("init_cells.txt")
@@ -14,22 +14,25 @@ grid.compute_states_rewards()
 grid.normalize_rewards()
 
 init_state = random.sample(init_states, 1)[0]
-greedy_thief = GreedyPathThief(grid)
-greedy_thief.value_iteration(debug=True)
+# greedy_thief = GreedyPathThief(grid)
+# greedy_thief.value_iteration(debug=True)
+probabilistic_thief = ProbabilisticGreedyPathThief(grid)
+probabilistic_thief.value_iteration(debug=True)
+
 
 valid_path = False
 max_tries = 1000
 current_try = 1
 while not valid_path:
-    valid_path = greedy_thief.compute_path(init_state)
+    valid_path = probabilistic_thief.compute_path(init_state)
     current_try += 1
     if current_try > max_tries:
         print(f"failed to find a valid path without loops. tried for {max_tries} iterations")
         break
 
 if valid_path:
-    path = greedy_thief.get_path()
-    path_weight = greedy_thief.get_path_weight()
+    path = probabilistic_thief.get_path()
+    path_weight = probabilistic_thief.get_path_weight()
     print(f"the path from init state {init_state} to some goal state is:\n{path}")
     print(f"the path total weight is {path_weight}")
     print(f"the path total length is {len(path)}")
--- SearchAndCapture/thief_algorithms.py
+++ SearchAndCapture/thief_algorithms.py
@@ -1,11 +1,9 @@
+import random
 import math
+import numpy as np
 
 
-class ValueIterationThief(object):
-
-    def __init__(self, grid_object):
-        self.grid = grid_object
-        self.v = {}
+class BaseValueIterationThief(object):
 
     @staticmethod
     def _compute_rms(v, new_v):
@@ -20,47 +18,11 @@ class ValueIterationThief(object):
 
         return math.sqrt(sum_squares) / num_states
 
-    def value_iteration(self, gamma=0.99, convergence_rate=0.0001, debug=False):
-        converged = False
-        states = self.grid.get_states()
-        states_rewards = self.grid.get_all_states_rewards()
-
-        v = states_rewards
-        num_iterations = 0
-
-        while not converged:
-            new_v = {}
-
-            for s in states:
-                neighbors = self.grid.get_state_neighbors(s)
-                neighbors_values = [v[n] for n in neighbors]
-
-                # Bellman Equation - where T(S,a,S') = 1 if S' is reachable from S
-                new_v[s] = states_rewards[s] + gamma * max(neighbors_values)
-
-            rms = self._compute_rms(v, new_v)
-            v = new_v
-
-            if debug:
-                num_iterations += 1
-                if num_iterations % 100 == 0:
-                    print(f'current iteration {num_iterations}')
-
-            if rms < convergence_rate:
-                converged = True
-                if debug:
-                    print(f'finished after iteration {num_iterations}')
-
-        self.v = v
-        return v
-
-
-class GreedyPathThief(ValueIterationThief):
-
     def __init__(self, grid_object):
-        super(GreedyPathThief, self).__init__(grid_object)
+        self.grid = grid_object
         self.path = []
         self.total_weight = 0
+        self.v = {}
 
     def get_path(self):
         return self.path
@@ -98,11 +60,99 @@ class GreedyPathThief(ValueIterationThief):
                 return True
 
             elif len(neighbors_values) > 0:
-                # choose best valued neighbor and move to it
-                best_neighbors, _ = max(neighbors_values, key=lambda x: x[1])
-                path.append(best_neighbors)
-                current_state = best_neighbors
+                # choose next state according to class policy
+                next_state = self.choose_next_state(neighbors_values)
+                path.append(next_state)
+                current_state = next_state
 
             else:
                 # if there is no valid neighbor - algorithm failed
+                print(path)
                 return False
+
+    def choose_next_state(self, neighbors_values):
+        raise NotImplementedError("A Sub class must implement the choose next state method")
+
+    def value_iteration(self, gamma=0.99, convergence_rate=0.0001, debug=False):
+        converged = False
+        states = self.grid.get_states()
+        states_rewards = self.grid.get_all_states_rewards()
+
+        v = states_rewards
+        num_iterations = 0
+
+        while not converged:
+            new_v = {}
+
+            for s in states:
+                neighbors = self.grid.get_state_neighbors(s)
+                neighbors_values = [v[n] for n in neighbors]
+
+                # Bellman Equation - where T(S,a,S') = 1 if S' is reachable from S
+                new_v[s] = states_rewards[s] + gamma * max(neighbors_values)
+
+            rms = self._compute_rms(v, new_v)
+            v = new_v
+
+            if debug:
+                num_iterations += 1
+                if num_iterations % 100 == 0:
+                    print(f'current iteration for value iteration algorithm: {num_iterations}')
+
+            if rms < convergence_rate:
+                converged = True
+                if debug:
+                    print(f'finished value iteration after {num_iterations} iterations')
+
+        self.v = v
+        return v
+
+
+class GreedyPathThief(BaseValueIterationThief):
+
+    def __init__(self, grid_object):
+        super(GreedyPathThief, self).__init__(grid_object)
+
+    def choose_next_state(self, neighbors_values):
+        # choose best neighbor according to states values
+        best_neighbor, _ = max(neighbors_values, key=lambda x: x[1])
+        return best_neighbor
+
+
+class ProbabilisticGreedyPathThief(BaseValueIterationThief):
+
+    def __init__(self, grid_object):
+        super(ProbabilisticGreedyPathThief, self).__init__(grid_object)
+
+    def normalize_states_values(self):
+        state_values = list(self.v.values())
+        values_mean = np.mean(state_values)
+        value_std = np.std(state_values)
+
+        self.v = {s: (self.v[s] - values_mean) / value_std for s in self.v}
+
+    def compute_path(self, init_state):
+        self.normalize_states_values()
+        super(ProbabilisticGreedyPathThief, self).compute_path(init_state)
+
+    def choose_next_state(self, neighbors_values):
+        values_sum = sum([np.exp(value) for n, value in neighbors_values])
+        values_distribution = [(n, np.exp(value) / values_sum) for n, value in neighbors_values]
+        # values_sum = sum([np.exp(value) if value >= 1e-10 else 0 for n, value in neighbors_values])
+        # values_distribution = [(n, np.exp(value) / values_sum) if value >= 1e-10 else (n, 0)
+        #                        for n, value in neighbors_values]
+        sorted_probabilities = sorted(values_distribution, key=lambda x: x[1], reverse=True)
+
+        probability = random.random()
+        next_state, next_state_prob = sorted_probabilities[0]
+        cumulative_probability = next_state_prob
+        for i in range(1, len(sorted_probabilities)):
+
+            if probability <= cumulative_probability:
+                return next_state
+
+            else:
+                next_state, next_state_prob = sorted_probabilities[i]
+                cumulative_probability += next_state_prob
+
+        return next_state
