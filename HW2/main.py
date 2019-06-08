cdb8ee5bd72fdb99c8b8a2ba5720980c56ee4d51
Ofri Kleinfeld <ofri64@gmail.com>
2019-06-08 17:27:23 +0300

    now tested with D feature maps

--- HW2/main.py
+++ HW2/main.py
@@ -7,15 +7,14 @@ if __name__ == '__main__':
     # w = np.random.rand(96, 3, 11, 11)
     # bias = np.random.rand(96)
     # util_functions.conv2d(x, w, bias)
-    x = np.array([[0, 0, 0, 0, 0], [0, 1, 2, 3, 0], [0, 4, 5, 6, 0], [0, 7, 8, 9, 0], [0, 0, 0, 0, 0]])
-    weight = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    x = np.array([[[0, 0, 0, 0, 0], [0, 1, 2, 3, 0], [0, 4, 5, 6, 0], [0, 7, 8, 9, 0], [0, 0, 0, 0, 0]]])
+    weight = np.array([[[[1, 2, 1], [0, 0, 0], [-1, -2, -1]]], [[[1, 2, 1], [0, 0, 0], [-1, -2, -1]]]])
     print(x)
     print(weight)
 
     stride = 1
-    C = 1
-    H, W = x.shape
-    w, h = weight.shape
+    C, H, W = x.shape
+    D, _, w, h = weight.shape
 
     w_out = (H - h) // stride + 1
     h_out = (W - w) // stride + 1
@@ -31,15 +30,15 @@ if __name__ == '__main__':
 
     k = np.repeat(np.arange(C), h * w).reshape(-1, 1)
 
-    cols = x[i, j]
+    cols = x[k, i, j]
     print(cols)
     print(cols.shape)
 
-    rows = weight.reshape(1, C * h * w)
+    rows = weight.reshape(D, C * h * w)
     print(rows.shape)
     print(rows)
 
     conv_res = rows @ cols
     print(conv_res.shape)
-    print(conv_res.reshape(C, h_out, w_out))
+    print(conv_res.reshape(D, h_out, w_out))
 
