f185285102d4d0456895f1c6b86cb1b529731cff
Ofri Kleinfeld <ofri64@gmail.com>
2019-06-22 22:50:05 +0300

    changed to sigmoid. adds saving the model capabilities

--- HW2/main.py
+++ HW2/main.py
@@ -18,22 +18,26 @@ if __name__ == '__main__':
 
     train_data = train_data.reshape(-1, 3, 32, 32)
     # train_data = train_data.reshape(-1, 3072)
-    train_data = train_data[:4000]
+    # train_data = train_data[:4000]
     train_length = len(train_data)
     validation_data = validation_data.reshape(-1, 3, 32, 32)
     # validation_data = validation_data.reshape(-1, 3072)
     valid_length = len(validation_data)
 
     model = networks.SimpleCNN()
-    optimizer = optimizers.SGDOptimizer(model, lr=0.01)
-    num_epochs = 10
+    optimizer = optimizers.SGDOptimizer(model, lr=0.3, weights_decay='L2', weights_decay_rate=0.001)
+    num_epochs = 50
     batch_size = 32
-    batch_indices = range(0, round((len(train_data)/batch_size)) * batch_size, batch_size)
+
+    validation_sample_size = 1000
+    train_batch_indices = range(0, round((len(train_data) / batch_size)) * batch_size, batch_size)
+    validation_batch_indices = range(0, round((validation_sample_size / batch_size)) * batch_size, batch_size)
+    best_validation_accuracy = 0
 
     for epoch in range(num_epochs):
         epoch_loss = 0
 
-        for k in batch_indices:
+        for k in train_batch_indices:
             x_batch = train_data[k: k + batch_size]
             y_batch = train_labels[k: k + batch_size]
 
@@ -50,7 +54,7 @@ if __name__ == '__main__':
 
         # evaluate accuracy on training set
         num_correct = 0
-        for k in batch_indices:
+        for k in train_batch_indices:
             x_batch = train_data[k: k + batch_size]
             y_batch = train_labels[k: k + batch_size]
 
@@ -64,14 +68,11 @@ if __name__ == '__main__':
 
         # evaluate accuracy on validation
         # choose randomly 1000 samples from validation set
-        sample_size = 1000
-        random_indices = np.random.choice(valid_length, sample_size)
+        random_indices = np.random.choice(valid_length, validation_sample_size)
         validation_sample = validation_data[random_indices, :, :, :]
         validation_sample_labels = validation_labels[random_indices]
 
         # generate batches predict and compare
-        validation_batch_indices = range(0, round((sample_size/batch_size)) * batch_size, batch_size)
-
         num_correct = 0
         for k in validation_batch_indices:
             x_batch = validation_sample[k: k + batch_size]
@@ -83,4 +84,9 @@ if __name__ == '__main__':
             batch_correct_predictions = np.sum(predictions == correct_labels)
             num_correct += batch_correct_predictions
 
-        print(f"Epoch {epoch + 1} - prediction accuracy on validation set is: {num_correct / sample_size}")
+        validation_accuracy = num_correct / validation_sample_size
+        if validation_accuracy > best_validation_accuracy:
+            load_data.save_data_as_pickle_gz(model, "cnn_lr0.3_all.txt")
+            best_validation_accuracy = validation_accuracy
+
+        print(f"Epoch {epoch + 1} - prediction accuracy on validation set is: {validation_accuracy}")
--- HW2/networks.py
+++ HW2/networks.py
@@ -5,31 +5,31 @@ class SimpleCNN(object):
 
     def __init__(self):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=5, kernel_size=3, stride=1)
-        self.relu1 = nn.Relu()
+        self.sigmoid1 = nn.Sigmoid()
         self.max_pool1 = nn.MaxPool2d(kernel_size=2)
         self.conv2 = nn.Conv2d(in_channels=5, out_channels=8, kernel_size=4, stride=1)
-        self.relu2 = nn.Relu()
+        self.sigmoid2 = nn.Sigmoid()
         self.max_pool2 = nn.MaxPool2d(kernel_size=2)
         self.conv3 = nn.Conv2d(in_channels=8, out_channels=12, kernel_size=3, stride=1)
-        self.relu3 = nn.Relu()
+        self.sigmoid3 = nn.Sigmoid()
         self.max_pool3 = nn.MaxPool2d(kernel_size=2)
         self.flatten = nn.Flatten()
         self.linear = nn.Linear(in_dimension=48, out_dimension=10)
         self.softmax = nn.Softmax()
-        self.layers = [self.conv1, self.relu1, self.max_pool1,
-                        self.conv2, self.relu2, self.max_pool2,
-                        self.conv3, self.relu3, self.max_pool3,
-                        self.flatten, self.linear, self.softmax]
+        self.layers = [self.conv1, self.sigmoid1, self.max_pool1,
+                       self.conv2, self.sigmoid2, self.max_pool2,
+                       self.conv3, self.sigmoid3, self.max_pool3,
+                       self.flatten, self.linear, self.softmax]
 
     def __call__(self, x):
         x = self.conv1(x)
-        x = self.relu1(x)
+        x = self.sigmoid1(x)
         x = self.max_pool1(x)
         x = self.conv2(x)
-        x = self.relu2(x)
+        x = self.sigmoid2(x)
         x = self.max_pool2(x)
         x = self.conv3(x)
-        x = self.relu3(x)
+        x = self.sigmoid3(x)
         x = self.max_pool3(x)
         x = self.flatten(x)
         x = self.linear(x)
@@ -42,13 +42,13 @@ class SimpleCNN(object):
         linear_grad = self.linear.backward(softmax_grad)
         flatten_grad = self.flatten.backward(linear_grad)
         max_pool3_grad = self.max_pool3.backward(flatten_grad)
-        relu3_grad = self.relu3.backward(max_pool3_grad)
+        relu3_grad = self.sigmoid3.backward(max_pool3_grad)
         conv3_grad = self.conv3.backward(relu3_grad)
         max_pool2_grad = self.max_pool2.backward(conv3_grad)
-        relu2_grad = self.relu2.backward(max_pool2_grad)
+        relu2_grad = self.sigmoid2.backward(max_pool2_grad)
         conv2_grad = self.conv2.backward(relu2_grad)
         max_pool1_grad = self.max_pool1.backward(conv2_grad)
-        relu1_grad = self.relu1.backward(max_pool1_grad)
+        relu1_grad = self.sigmoid1.backward(max_pool1_grad)
         conv1_grad = self.conv1.backward(relu1_grad)
 
 
